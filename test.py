bda5a00bc47fd8531f3f569767ba90461e6a66ab
Hassaan Shariq <hassaankhizar111@gmail.com>
2026-01-13 22:11:29 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,59 @@
-#a = 'i will prove them wrong no matter what'
-#b = ' ,  i will do it for my mom'
-#print(a , b)
+a = 'i will prove them wrong no matter what' # concatenate two strings
+b = ' ,  i will do it for my mom'
+print(a , b)
 
-# len("hassaan")            # use ctrl +forwaed slash / to comment out the whole code
-# print(len("hassaan"))
+len("hassaan")     # LENGHTH
 
-# word ="python"
-# print(word[3])  # indexing
+word ="python"  # INDEXING
+print(word[3])  
 
-# print("AB\nC\nDE")
-
-
-# word = "hello"
-# uppercase_word = word.upper()
-# print(uppercase_word)
-
-# j = "5"
-# k = "6"
-# l = j + k
-# print(l)
+                                   # use ctrl +forwaed slash / to comment out the whole code
+print("AB\nC\nDE")                  
 
 
-# morning_temp = 22
-# evening_temp = morning_temp - 3
-# print(evening_temp)
+word = "hello"
+uppercase_word = word.upper()  # UPPERCASEING
+print(uppercase_word)
 
-# subtotal = 40 + 12
-# service_charge = subtotal * 0.15
-# final_bill = subtotal + service_charge
-# print(final_bill)
+j = "5"  # CONCATENATION
+k = "6"
+l = j + k
+print(l)
 
-# result = "5" + "6"
-# print(result)
 
-# word = "python"
-# uppercase_word = word.upper()
-# print(uppercase_word)
+morning_temp = 22        # TEMPERATURE DIFFERENCE
+evening_temp = morning_temp - 3
+print(evening_temp)
 
-name = "hassaan"
+subtotal = 40 + 12        # BILL CALCULATION
+service_charge = subtotal * 0.15
+final_bill = subtotal + service_charge
+print(final_bill)
+
+result = "5" + "6"
+print(result)
+
+word = "python"    # UPPERCASEING
+uppercase_word = word.upper()
+print(uppercase_word)
+
+name = "hassaan"    # SLICING
 print(name[0:5:2])
 
-name = "hassaan"
-believe = "hassaan is going to become a billionaire"
+name = "hassaan"    # CONCATENATION
+believe = "hassaan is going to become a billionaire" 
 truth = name + "" + believe 
 print(truth)
+
+sentence = "I will become a full stack developer inshaAllah"  # SPLITTING A SENTENCE INTO WORDS
+word = sentence.split()
+print(word)
+
+text = "Consistency is the door to success\'remember that\'"   # REPLACING A WORD
+corrected_text = text.replace("door", "key")
+print(corrected_text)
+
+quote = ''' Steve Jobs highlighted that those "crazy enough" to think they can change the world
+are ultimately the ones who do. '''
+line = quote.split()         # SPLITTING A SENTENCE INTO WORDS
+print(line)
