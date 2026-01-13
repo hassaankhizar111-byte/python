b8aceccaa6e732c587acb2ff624b9b92170d22ea
Hassaan Shariq <hassaankhizar111@gmail.com>
2026-01-13 21:37:14 +0500

    Update strings_functions.py

--- strings_functions.py
+++ strings_functions.py
@@ -1,13 +1,28 @@
-name = "hassaan"    # LEN = LENGHTH
+name = "Python"    # LEN = LENGHTH
 print(len(name))
-line = "i will become the python developer insha allah"
+line = "i will become the full stack developer insha allah"
 print(len(line))
 
 word = "python"     # UPPER = UPPERCASEING
 uppercase_word = word.upper()
 print(uppercase_word)
 
+word = "PYTHON"    # LOWER = LOWERCASEING
+lowercase_word = word.lower()
+print(lowercase_word)
 
+phrase = "   hello world   "   # STRIP = REMOVING WHITESPACES
+stripped_phrase = phrase.strip()
+print(stripped_phrase)
 
+line = "welcome to the world of java programming"   # REPLACE = REPLACING A WORD
+second_line = line.replace("java", "python") 
+print(second_line)
 
+text = "i love coding" # FIND = FINDING A WORD
+index = text.find("coding")
+print(index)
 
+statement = "i love python programming" # SPLIT = SPLITTING A SENTENCE INTO WORDS 
+words = statement.split()
+print(words) 
