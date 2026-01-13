338d68a4035021bdd8f62bfcd4c02159e3bc52ed
Hassaan Shariq <hassaankhizar111@gmail.com>
2026-01-13 16:26:09 +0500

    Update input_function.py

--- input_function.py
+++ input_function.py
@@ -1,10 +1,12 @@
-a = input("book author : ")
-a = int(a) # coverting 'a' into an integer
-print(type(a))
-# first = input("First name: hassaan ")
-# last = input("Last name: shariq ")
-# username = first.lower() + "_" + last.lower()
-# print("Generated username:", username)
+first = input("First name: hassaan ")
+last = input("Last name: shariq ")
+username = first.lower() + "_" + last.lower()
+print("Generated username:", username)
 
 name = input("Enter your name: ")
 print("Hello, " + name + "! Welcome to the program.")
+
+age = input("Enter your age: ")
+next_month_age = int(age) + 1
+sentence = "Next month, you will be " + str(next_month_age) + " years old."
+print(sentence)
